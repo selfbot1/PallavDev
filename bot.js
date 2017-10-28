c3e6de4698ba9771b54f8e339be531a53ff7d569
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-28 16:59:06 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require("./config.json");
+
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
+    if(message.author.id !== config.ownerID) return;
+
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(config.token);
