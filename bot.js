b65dc10972053e523a15711683d739f34dbd462f
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-28 21:41:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -136,15 +136,9 @@ client.on("message", (message) => {
       
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    message.channel.fetchMessages({limit: deletecount}).then(messages => message.channel.bulkDelete(messages));
+
+   }
   }
   if(command === "eyes") {
       //This command send emoji the looks left and read
