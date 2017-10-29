1a6a1e95fb72b559c8a2f4528dd1b8c987a1b345
PallavBS <32361541+PallavBS@users.noreply.github.com>
2017-10-28 22:14:19 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ client.on("message", (message) => {
     // Now, time for a swift kick in the nuts!
     member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    message.channel.send(`${member.user.tag} has been kicked by ${message.author} because: ${reason}`);
 
   }
   
